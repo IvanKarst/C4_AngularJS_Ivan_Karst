52435db3d8359a7dd2676601f5e014664930a7ae
Ivan Karst <ivankarst16@gmail.com>
2023-09-21 12:30:45 +0200

    modified:   Inleveropdrachten/opdracht a/inc/model.js

--- Inleveropdrachten/opdracht a/inc/model.js
+++ Inleveropdrachten/opdracht a/inc/model.js
@@ -12,7 +12,7 @@ var regex = Array[
 	name_reg		= "/^[A-Z][a-z'-]+$/",
 	street_reg		= "/^[A-z-]+$/",
 	number_reg		= "/^\d{1,4}(\s?)[a-z]{0,1}",
-	postal_reg		= "/^\d{4}\s?([A-Z]{2})$/",
+	postal_reg		= "/^\d{4}\s?([A-Z]{2}|[a-z]{2})$/",
 	place_reg		= "/^[A-z\s-]+$/",
 	emailadress		= "/^([A-z0-9.-])+@([A-z0-9.-])+\.([A-z0-9.-]){2,4}$/"
 ];
