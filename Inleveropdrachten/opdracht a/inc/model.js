d8f2beb3f61da2d7893df6ce4a7c05cbb171b86f
Ivan Karst <ivankarst16@gmail.com>
2023-09-21 11:00:52 +0200

    modified:   Inleveropdrachten/opdracht a/inc/model.js

--- Inleveropdrachten/opdracht a/inc/model.js
+++ Inleveropdrachten/opdracht a/inc/model.js
@@ -1,7 +1,18 @@
-var binder = Array[
-    firstName   = "John",
-    lastName    = "Doe",
-    street      = "Street",
-    housenumber = "123a",
-    postalCode  = "1234 AB"
+var base = Array[
+	firstName   	= "John",
+	lastName		= "Doe",
+	street			= "Streetname",
+	housenumber		= "123a",
+	postalCode		= "1234 AB",
+	place			= "Place",
+	emailadress		= "johndoe123@hotmail.com"
+];
+
+var regex = Array[
+	name_reg		= "/^[A-Z][a-z'-]+$/",
+	street_reg		= "/^[A-Z][a-z-]+$/",
+	number_reg		= "/^\d{1,4}(\s?)[a-z]{0,1}",
+	postal_reg		= "/^\d{4}\s?([A-Z]{2})$/",
+	place_reg		= "/^[A-Z][a-z-]+$/",
+	emailadress		= "/^([A-z0-9.-])+@([A-z0-9.-])+\.([A-z0-9.-]){2,4}$/"
 ];
