4f4b8cd0b4d3eb5adfde618c01abdffae3d26918
Ivan Karst <ivankarst16@gmail.com>
2023-09-22 10:35:35 +0200

    modified:   Inleveropdrachten/opdracht a/inc/controller.js 	modified:   Inleveropdrachten/opdracht a/index.html

--- Inleveropdrachten/opdracht a/inc/controller.js
+++ Inleveropdrachten/opdracht a/inc/controller.js
@@ -1,10 +1,27 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
 	$scope.render = function() {
-		const parentElement = document.querySelector('.binder');
-		const childElement = document.createElement('div');
-		childElement.setAttribute('class', 'data row');
-		parentElement.appendChild(childElement);
+		// sets row
+		const parent = document.querySelector('.binder');
+		const child = document.createElement('div');
+		child.setAttribute('class', 'data row');
+		parent.appendChild(childElement);
+		// sets all outcomes
+		const table = document.querySelector('.data.row');
+		cell = document.createElement('div');
+		childElement.setAttribute('class', 'col-1');
+		table.appendChild(childElement); 
+		
+	}
+
+	// Grabs data based on input
+	$scope.data_grabber = function() {
+		alert(base[firstName]);
+	}
+
+	// Validates data requested by the grabber
+	$scope.data_validator = function() {
+
 	}
 });
 
