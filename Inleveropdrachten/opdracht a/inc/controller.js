633c27f7359594838588dd3a2fc7eae641804773
Ivan Karst <ivankarst16@gmail.com>
2023-09-28 09:38:56 +0200

    modified:   Inleveropdrachten/opdracht a/inc/controller.js

--- Inleveropdrachten/opdracht a/inc/controller.js
+++ Inleveropdrachten/opdracht a/inc/controller.js
@@ -1,12 +1,7 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
 	$scope.render = function() {
-		// sets all outcomes
-		// const table = document.querySelector('.binder');
-		// cell = document.createElement('div');
-		// cell.setAttribute('class', 'col-1');
-		// table.appendChild(cell); 
-		console.log(data)
+		
 	}
 
 	// Grabs data based on input
