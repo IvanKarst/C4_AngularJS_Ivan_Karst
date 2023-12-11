390836915e94e0d9db93b68be955680e24368aec
Ivan Karst <ivankarst16@gmail.com>
2023-12-11 13:06:13 +0100

    Changes to be committed: 	modified:   Inleveropdrachten/opdracht b/inc/controller.js 	modified:   Inleveropdrachten/opdracht b/inc/master.css 	modified:   Inleveropdrachten/opdracht b/index.html

--- Inleveropdrachten/opdracht b/inc/controller.js
+++ Inleveropdrachten/opdracht b/inc/controller.js
@@ -1,5 +1,13 @@
 app.controller('myCtrl', function($scope, cars) {
-	$scope.sort = function(sortBy) {
-		
+	$scope.cars = angular.copy(cars.cars);
+	$scope.sortBy = '';
+
+	$scope.sort = function(prop) {
+		if ($scope.sortBy === prop && $scope.reverse == false) {
+			$scope.reverse = !$scope.reverse;
+		} else {
+			$scope.sortBy = prop;
+			$scope.reverse = false;
+		}
 	};
 });
