f75d995fcdec97f8c90fbeef653bf629f5f31c97
Ivan Karst <ivankarst16@gmail.com>
2023-12-11 11:23:33 +0100

    modified:   Inleveropdrachten/opdracht b/inc/controller.js 	modified:   Inleveropdrachten/opdracht b/inc/model.js 	modified:   Inleveropdrachten/opdracht b/index.html

--- Inleveropdrachten/opdracht b/inc/model.js
+++ Inleveropdrachten/opdracht b/inc/model.js
@@ -3,41 +3,41 @@ const app = angular.module('myApp', [])
 app.factory('cars', function () {
 	return {
 		cars: [
-            {
-                kenteken: 'AB-123-CD',
-                kleur: 'Red',
-                merk: 'Toyota',
-                aantal_deuren: '3',
-                prijs: 15000
-            },
-            {
-                kenteken: 'XY-987-ZW',
-                kleur: 'Blue',
-                merk: 'Honda',
-                aantal_deuren: '5',
-                prijs: 20000
-            },
-            {
-                kenteken: 'GH-456-JK',
-                kleur: 'Black',
-                merk: 'Ford',
-                aantal_deuren: '3',
-                prijs: 18000
-            },
-            {
-                kenteken: 'MN-789-OP',
-                kleur: 'Green',
-                merk: 'Chevrolet',
-                aantal_deuren: '5',
-                prijs: 22000
-            },
-            {
-                kenteken: 'IJ-654-UV',
-                kleur: 'Silver',
-                merk: 'Nissan',
-                aantal_deuren: '3',
-                prijs: 19000
-            }
-        ]
+			{
+				kenteken: 'AB-123-CD',
+				kleur: 'Rood',
+				merk: 'Toyota',
+				aantal_deuren: '3',
+				prijs: 15000
+			},
+			{
+				kenteken: 'XY-987-ZW',
+				kleur: 'Blauw',
+				merk: 'Honda',
+				aantal_deuren: '5',
+				prijs: 20000
+			},
+			{
+				kenteken: 'GH-456-JK',
+				kleur: 'Zwart',
+				merk: 'Ford',
+				aantal_deuren: '3',
+				prijs: 18000
+			},
+			{
+				kenteken: 'MN-789-OP',
+				kleur: 'Groen',
+				merk: 'Chevrolet',
+				aantal_deuren: '5',
+				prijs: 22000
+			},
+			{
+				kenteken: 'IJ-654-UV',
+				kleur: 'Zilver',
+				merk: 'Nissan',
+				aantal_deuren: '3',
+				prijs: 19000
+			}
+		]
 	};
 });
