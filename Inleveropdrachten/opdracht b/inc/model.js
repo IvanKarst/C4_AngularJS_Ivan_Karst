bdcac67bc87278a2a875f9a6296a1c37a9fc18f6
Ivan Karst <ivankarst16@gmail.com>
2023-12-11 13:09:38 +0100

    modified:   Inleveropdrachten/opdracht b/inc/model.js

--- Inleveropdrachten/opdracht b/inc/model.js
+++ Inleveropdrachten/opdracht b/inc/model.js
@@ -4,35 +4,35 @@ app.factory('cars', function () {
 	return {
 		cars: [
 			{
-				kenteken: 'AB-123-CD',
+				kenteken: 'AB-12-CD',
 				kleur: 'Rood',
 				merk: 'Toyota',
 				aantal_deuren: '3',
 				prijs: 15000
 			},
 			{
-				kenteken: 'XY-987-ZW',
+				kenteken: 'X-789-YZ',
 				kleur: 'Blauw',
 				merk: 'Honda',
 				aantal_deuren: '5',
 				prijs: 20000
 			},
 			{
-				kenteken: 'GH-456-JK',
+				kenteken: 'PQ-456-R',
 				kleur: 'Zwart',
 				merk: 'Ford',
 				aantal_deuren: '3',
 				prijs: 18000
 			},
 			{
-				kenteken: 'MN-789-OP',
+				kenteken: 'JK-789-L',
 				kleur: 'Groen',
 				merk: 'Chevrolet',
 				aantal_deuren: '5',
 				prijs: 22000
 			},
 			{
-				kenteken: 'IJ-654-UV',
+				kenteken: 'Y-123-AB',
 				kleur: 'Zilver',
 				merk: 'Nissan',
 				aantal_deuren: '3',
