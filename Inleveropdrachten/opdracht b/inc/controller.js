7b5602b8eb9aec1e6bdcecdaec18d6e20c3184cf
Ivan Karst <ivankarst16@gmail.com>
2023-12-11 10:30:35 +0100

    modified:   Inleveropdrachten/opdracht b/inc/controller.js 	modified:   Inleveropdrachten/opdracht b/index.html

--- Inleveropdrachten/opdracht b/inc/controller.js
+++ Inleveropdrachten/opdracht b/inc/controller.js
@@ -1,3 +1,10 @@
 app.controller('myCtrl', function($scope, cars) {
-	
+    $scope.updateTable = function(newCar) {
+        // Add the new car to the cars array
+        $scope.cars.push(angular.copy(newCar));
+
+        // Clear the form after submission
+        $scope.newCar = {};
+        // You can also save this data to your backend or update your model.js file if needed
+    };
 });
