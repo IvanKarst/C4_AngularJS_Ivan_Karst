58a9d0a0015e5d1f2c29c22c621596e8d159e199
Ivan Karst <ivankarst16@gmail.com>
2023-12-15 12:44:23 +0100

    modified:   inc/config.js

--- inc/config.js
+++ inc/config.js
@@ -1,15 +1,15 @@
 app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "templates/main.htm"
-    })
-    .when("/red", {
-        templateUrl : "red.htm"
-    })
-    .when("/green", {
-        templateUrl : "green.htm"
-    })
-    .when("/blue", {
-        templateUrl : "blue.htm"
-    });
+	$routeProvider
+	.when("/", {
+		templateUrl : "templates/main.htm"
+	})
+	.when("/red", {
+		templateUrl : "red.htm"
+	})
+	.when("/green", {
+		templateUrl : "green.htm"
+	})
+	.when("/blue", {
+		templateUrl : "blue.htm"
+	});
 });
