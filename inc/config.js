7855260ccf68b80470b0ae982f09d93b0874fc61
Ivan Karst <ivankarst16@gmail.com>
2023-12-15 13:41:41 +0100

    modified:   inc/config.js 	modified:   inc/controller.js

--- inc/config.js
+++ inc/config.js
@@ -1,15 +1,15 @@
 app.config(function($routeProvider) {
 	$routeProvider
 	.when("/", {
-		templateUrl : "templates/main.htm"
+	  templateUrl : "templates/main.htm"
 	})
-	.when("/red", {
-		templateUrl : "templates/red.htm"
+	.when("/update", {
+	  templateUrl : "templates/update.htm"
 	})
 	.when("/green", {
-		templateUrl : "templates/green.htm"
+	  templateUrl : "templates/green.htm"
 	})
 	.when("/blue", {
-		templateUrl : "templates/blue.htm"
-	})
-});
+	  templateUrl : "templates/blue.htm"
+	});
+  });
--- inc/controller.js
+++ inc/controller.js
@@ -1,4 +1,4 @@
-app.controller('myCtrl', function($scope, $http) {
+app.controller('myCtrl', function($scope, $http, $routeParams, $location) {
 	$http.get("php/auto.php").then(
 		function (response) {
 			$scope.names = response.data.records;
