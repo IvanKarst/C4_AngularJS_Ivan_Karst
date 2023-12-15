c0bc1c063ecc4c93e168c00a146332c1a62937ba
Ivan Karst <ivankarst16@gmail.com>
2023-12-15 12:34:37 +0100

    modified:   php/auto.php

--- php/auto.php
+++ php/auto.php
@@ -3,23 +3,23 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=UTF-8');
 include('model.php');
 
-
-$outp = array();
-
 if($conn->connect_error){
 	die('Connection failed.<br/> Error: '. $conn->connect_error);
 }
 
-$result = $conn->query('SELECT * FROM Customers');
+function get_data($items, $table){
+	global $conn;
+	$result = $conn->query('SELECT '.$items.' FROM '. $table);
 
-if(!$result){
-	die('Query failed.<br/> Error: '.$conn->error);
+	if(!$result){
+		die('Query failed.<br/> Error: '.$conn->error);
+	}
+
+	while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+		$outp[] = $rs;
+	}
+
+	$conn->close();
+
+	echo json_encode(array("records" => $outp));
 }
-
-while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-	$outp[] = $rs;
-}
-
-$conn->close();
-
-echo json_encode(array("records" => $outp));
