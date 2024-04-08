3a789611f0f530e79a1fb8dee1c87a3bd22622b4
Ivan Karst <ivankarst16@gmail.com>
2024-04-08 09:48:44 +0200

    modified:   inc/config.js 	new file:   templates/delete_person.htm

--- inc/config.js
+++ inc/config.js
@@ -1,12 +1,15 @@
-app.config(function($routeProvider) {
-	$routeProvider
-	.when("/", {
-	  templateUrl : "templates/home.htm"
-	})
-	.when("/alter_data", {
-	  templateUrl : "templates/alter_data.htm"
-	})
-	.when("/add_person", {
-	  templateUrl : "templates/add_person.htm"
-	});
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "templates/home.htm",
+    })
+    .when("/alter_data", {
+      templateUrl: "templates/alter_data.htm",
+    })
+    .when("/add_person", {
+      templateUrl: "templates/add_person.htm",
+    })
+    .when("/delete_person", {
+      templateUrl: "templates/delete_person.htm",
+    });
 });
